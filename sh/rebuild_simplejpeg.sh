19000fcbc1e1a336d519b94192de106800790377
choeyujeong911 <yujeongchoe20@gmail.com>
2026-07-30 09:06:25 -0400

    modified

--- sh/rebuild_simplejpeg.sh
+++ sh/rebuild_simplejpeg.sh
@@ -1,2 +1,3 @@
-sudo apt install -y cmake nasm build-essential python3-dev
+sudo apt install -y cmake nasm build-essential python3-dev libjpeg-dev
+python -m pip install --upgrade pip setuptools wheel cython
 python -m pip install --no-cache-dir --no-deps --no-binary=simplejpeg --no-build-isolation --force-reinstall simplejpeg
