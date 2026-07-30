83625766e906f201e494d1944b05361f85bb7b92
choeyujeong911 <yujeongchoe20@gmail.com>
2026-07-30 08:10:05 -0400

    Add shell scripts

---
+++ sh/change_mirror_repo.sh
@@ -0,0 +1,14 @@
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+sudo tee /etc/apt/sources.list >/dev/null <<'EOF'
+deb https://ftp.kaist.ac.kr/debian bookworm main contrib non-free non-free-firmware
+deb https://ftp.kaist.ac.kr/debian-security bookworm-security main contrib non-free non-free-firmware
+deb https://ftp.kaist.ac.kr/debian bookworm-updates main contrib non-free non-free-firmware
+EOF
+
+sudo sed -i \
+'s|http://archive.raspberrypi.com|https://archive.raspberrypi.com|' \
+/etc/apt/sources.list.d/raspi.list
+
+sudo apt clean
+sudo apt update
---
+++ sh/rebuild_simplejpeg.sh
@@ -0,0 +1,2 @@
+sudo apt install -y cmake nasm build-essential python3-dev
+python -m pip install --no-cache-dir --no-deps --no-binary=simplejpeg --no-build-isolation --force-reinstall simplejpeg
